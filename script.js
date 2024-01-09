88e9390628ea6d56948e06935f2762c9e7530fcf
Melike Özge Terzi <o_7@Melikes-MacBook-Air.local>
2024-01-09 14:22:38 +0300

    update

--- script.js
+++ script.js
@@ -386,7 +386,7 @@ menuBtn.addEventListener("click", function () {
     navbar.classList.toggle("active");
     document.addEventListener("click", function (e) {
       if (
-        !e.composedPath().includes(menuBtn) &&
+        !e.composedPath().includes(menu-btn) &&
         !e.composedPath().includes(navbar)
       ) {
         navbar.classList.remove("active");
